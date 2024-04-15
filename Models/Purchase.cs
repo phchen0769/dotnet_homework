c7e78683244f5b3facf6d4fb44732afca2124a38
Fedorov <phchen0769@icloud.com>
2024-04-15 20:58:40 +0800

    创建lab4

---
+++ Models/Purchase.cs
@@ -0,0 +1,17 @@
+using System;
+namespace BookStore.Models
+{
+    public class Purchase
+    {
+        // 购买ID
+        public int PurchaseId { get; set; }
+        // 购买人
+        public string Person { get; set; }
+        // 购买人地址
+        public string Address { get; set; }
+        //  书本ID
+        public int BookId { get; set; }
+        // 购买日期
+        public DateTime Date { get; set; }
+    }
+}
